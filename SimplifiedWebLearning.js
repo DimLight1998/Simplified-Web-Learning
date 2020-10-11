28224991c809fef25e0e215d9bbe7b9ca1149934
zhang-yang <dimlight1998@gmail.com>
2020-10-11 09:36:07 +0800

    make it work again

--- SimplifiedWebLearning.js
+++ SimplifiedWebLearning.js
@@ -4,10 +4,10 @@
 // @version      0.1
 // @description  remove useless elements on web learning front page
 // @author       DimLight1998
-// @match        http://learn.tsinghua.edu.cn/f/wlxt/index/course/student/
+// @match        https://learn.tsinghua.edu.cn/f/wlxt/index/course/student/
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js
-// @run-at document-idle
+// @run-at       document-idle
 // ==/UserScript==
 
 /* global $, _ */
@@ -18,7 +18,7 @@ function addStyle(css) {
     t.appendChild(newNode);
 }
 
-(function () {
+setTimeout(function () {
     'use strict';
     let cssLinks = [
         'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/label.min.css',
@@ -36,6 +36,9 @@ function addStyle(css) {
     let notices = courses.map((_i, e) => directText($(e).find('span.orange'))).toArray();
     let materials = courses.map((_i, e) => directText($(e).find('span.wee'))).toArray();
     let homeworks = courses.map((_i, e) => directText($(e).find('span.green'))).toArray();
+    let noticesTotal = courses.map((_i, e) => /.*?(\d+)/.exec(directText($(e).find('ul > li:nth-child(2) > p > span')))[1]).toArray();
+    let materialsTotal = courses.map((_i, e) => /.*?(\d+)/.exec(directText($(e).find('ul > li:nth-child(3) > p > span')))[1]).toArray();
+    let homeworksTotal = courses.map((_i, e) => /.*?(\d+)/.exec(directText($(e).find('ul > li:nth-child(4) > p > span')))[1]).toArray();
     let noticesLinks = courses.map((_i, e) => $(e).find('ul > li:nth-child(2) > a').attr('href')).toArray();
     let materialsLinks = courses.map((_i, e) => $(e).find('ul > li:nth-child(3) > a').attr('href')).toArray();
     let homeworksLinks = courses.map((_i, e) => $(e).find('ul > li:nth-child(4) > a').attr('href')).toArray();
@@ -47,22 +50,22 @@ function addStyle(css) {
         </td>
         <td class="ui center aligned">
             <a href="<%= noticeLink %>">
-                <div class="ui <%= parseInt(notice) === 0 ? '' : 'orange' %> label">
-                    <%= notice %>
+                <div class="ui <%= parseInt(notice) === 0 ? '' : 'orange' %> label" style="width: 5em;">
+                    <%= notice %> / <%= noticeTotal %>
                 </div>
             </a>
         </td>
         <td class="ui center aligned">
             <a href="<%= materialLink %>">
-                <div class="ui <%= parseInt(material) === 0 ? '' : 'orange' %> label">
-                    <%= material %>
+                <div class="ui <%= parseInt(material) === 0 ? '' : 'orange' %> label" style="width: 5em;">
+                    <%= material %> / <%= materialTotal %>
                 </div>
             </a>
         </td>
         <td class="ui center aligned">
             <a href="<%= homeworkLink %>">
-                <div class="ui <%= parseInt(homework) === 0 ? '' : 'orange' %> label">
-                    <%= homework %>
+                <div class="ui <%= parseInt(homework) === 0 ? '' : 'orange' %> label" style="width: 5em;">
+                    <%= homework %> / <%= homeworkTotal %>
                 </div>
             </a>
         </td>
@@ -83,14 +86,16 @@ function addStyle(css) {
     `);
 
     let rows = _.reduce(_.map(_.zip(
-        links, names, notices, materials, homeworks, noticesLinks, materialsLinks, homeworksLinks
+        links, names, notices, materials, homeworks, noticesLinks, materialsLinks, homeworksLinks, noticesTotal, materialsTotal, homeworksTotal
     ), o => row({
         link: o[0],
         name: /(.*)(\(\d+-\d+\))/.exec(o[1])[1],
         notice: o[2], material: o[3], homework: o[4],
-        noticeLink: o[5], materialLink: o[6], homeworkLink: o[7]
+        noticeLink: o[5], materialLink: o[6], homeworkLink: o[7],
+        noticeTotal: o[8], materialTotal: o[9], homeworkTotal: o[10]
     })), (a, b) => a + b);
     let content = table({ body: rows });
 
     $('#suoxuecourse').replaceWith(content);
-})();
+}, 1000);
+
